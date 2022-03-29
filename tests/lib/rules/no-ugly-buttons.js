8d61fb4f362fa4193bb6f8f822ba32fb608aa565
Jip Irfandy <irfandyjip98@gmail.com>
2022-03-29 21:37:07 +0700

    tests(rules): add valid/invalid cases for `no-ugly-buttons.js`

--- tests/lib/rules/no-ugly-buttons.js
+++ tests/lib/rules/no-ugly-buttons.js
@@ -11,21 +11,40 @@
 const rule = require("../../../lib/rules/no-ugly-buttons"),
   RuleTester = require("eslint").RuleTester;
 
+// Notice that we need to specify that we will be using JSX via `RuleTester.setDefaultConfig`.
+// This will allow our rule to be tested with a parser that knows about JSX.
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  }
+});
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
+const ERROR_MSG_NOT_STYLED = 'Buttons must be styled with a btn class at least.';
+
 const ruleTester = new RuleTester();
 ruleTester.run("no-ugly-buttons", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      code: '<button className="btn"></button>'
+    }
   ],
 
   invalid: [
     {
       code: "<button></button>",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      errors: [
+        {
+          message: ERROR_MSG_NOT_STYLED,
+          type: "JSXOpeningElement"
+        }
+      ],
     },
   ],
 });
